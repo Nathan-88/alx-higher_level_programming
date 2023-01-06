3ce96c60664f6f5cc5c6468975e011e2dced55bd
Nathan-88 <onwukaebuka88@gmail.com>
2023-01-06 04:25:05 -0800

    print a combination of two digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print(f"{i:02d}", end=", ")
-print("99\n")
+for i in range(100):
+    if i < 99:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i))
