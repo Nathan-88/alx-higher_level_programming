956ca920a92b1a8448441fcf6ea2dff3f64d14b1
Nathan-88 <onwukaebuka88@gmail.com>
2023-01-06 04:17:48 -0800

    prints a combination of two digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-for i in range(00, 100):
-    if i < 10:
-        i = str(i).zfill(2)
-    print("{}".format(i), end="")
-    if i != 99:
-        print(", ", end="")
-exit("\n")
+for i in range(0, 99):
+    print(f"{i:02d}", end=", ")
+print("99\n")
