5077e08fec2633e8a35fc80dde7da9d5403c6873
Nathan-88 <onwukaebuka88@gmail.com>
2023-01-09 20:14:09 -0800

    checks for lowercase aplhabets

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -4,10 +4,3 @@ def islower(c):
         return True
     else:
         return False
-
-
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
