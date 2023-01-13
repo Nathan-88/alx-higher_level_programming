a4749cc0ff6eb462469e230a53ae80607536e4bf
Nathan-88 <onwukaebuka88@gmail.com>
2023-01-12 20:11:34 -0800

    passing arguments from command line

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-
+import sys
+n = len(sys.argv)
+if n == 1:
+    print("{} arguments.".format(n-1))
+if n == 2:
+    print("{} argument:".format(n-1))
+    print("{}: {}".format(n-1, sys.argv[1]))
+if n > 2:
+    print("{} arguments:".format(n-1))
+    for i in range(1, n):
+        print("{}: {}".format(i, sys.argv[i]))
