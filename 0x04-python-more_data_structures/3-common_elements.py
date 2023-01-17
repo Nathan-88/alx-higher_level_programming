33130b8a14c0f0696a414bc6a5231d0733b374a3
Nathan-88 <onwukaebuka88@gmail.com>
2023-01-17 04:15:58 -0800

    a function that returns a set of common elements in two sets

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    com = ""
-    for i in set_1:
-        for j in set_2:
-            if j == i:
-                com = j
-    return com
+    new = list(set(set_1)&set(set_2))
+    return(new)
