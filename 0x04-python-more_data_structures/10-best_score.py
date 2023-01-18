ea8e85567c203b34775f472b7a62892ede7ec04a
Nathan-88 <onwukaebuka88@gmail.com>
2023-01-17 22:32:41 -0800

    max_value = max(a_dictionary.values())

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,8 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return(max(k for k, v in a_dictionary.items()))
+    max_value = max(a_dictionary.values())
+    for k, v in a_dictionary.items():
+        if v == max_value:
+            return k
+    return k
