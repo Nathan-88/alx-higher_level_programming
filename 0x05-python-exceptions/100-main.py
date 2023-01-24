ce988b4ad1e3ccf9fb6e235b0426821660994e47
Nathan-88 <onwukaebuka88@gmail.com>
2023-01-24 07:49:47 -0800

    source code for task 100-safe_print_integer_err

---
+++ 0x05-python-exceptions/100-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+value = 89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer_err(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
