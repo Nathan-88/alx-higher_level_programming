9e1a7b773ce1eea2959e984ed09403face650b2a
Nathan-88 <onwukaebuka88@gmail.com>
2023-01-31 05:28:42 +0100

    a class Rectangle that defines a rectangle by: (based on 1-rectangle.py)

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -52,4 +52,6 @@ class Rectangle:
         """public method
         Returns: perimeter of a rectangle
         """
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (2 * self.__width) + (2 * self.__height)
