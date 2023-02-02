98432b255eadcd5a3c2b65a822aef15a6e39cc0b
Nathan-88 <onwukaebuka88@gmail.com>
2023-02-02 12:04:41 +0100

    returns a new Rectangle instance with width == height == size

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -88,3 +88,7 @@ class Rectangle:
             return rect_1
         if rect_2.area() > rect_1.area():
             return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)
