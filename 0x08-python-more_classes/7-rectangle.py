9042414e648966ef91f2b7b2b2bafb78bd0081ca
Nathan-88 <onwukaebuka88@gmail.com>
2023-02-02 09:51:20 +0100

    Change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -64,8 +64,8 @@ class Rectangle:
         """defines string representation of Rectangle"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return '\n'.join(str(self.print_symbol) * \
-                self.__width for i in range(self.__height))
+        return '\n'.join(str(self.print_symbol) * self.__width
+                         for i in range(self.__height))
 
     def __repr__(self):
         """defines an ambigious string representation of an object"""
