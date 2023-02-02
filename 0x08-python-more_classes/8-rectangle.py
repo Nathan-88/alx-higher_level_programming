6d1ad72078ef4de53f82e2554bf87dd40bb04627
Nathan-88 <onwukaebuka88@gmail.com>
2023-02-02 12:08:32 +0100

    returns the biggest rectangle based on the area

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -80,6 +80,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """returns the biggest rectangle based on the area"""
         if isinstance(rect_1, Rectangle) is False:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if isinstance(rect_2, Rectangle) is False:
