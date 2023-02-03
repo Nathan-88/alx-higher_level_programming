094fa427821539e8c83583caa98f5f21c63df70e
Nathan-88 <onwukaebuka88@gmail.com>
2023-02-03 07:59:21 +0100

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -52,8 +52,8 @@ class Square:
         if self.__size == 0:
             print("")
             return
-
-        [print("") for i in range(0, self.__position[1])]
+        for i in range(self.__position[1]):
+            print("")
         for i in range(self.__size):
             for j in range(self.__position[0]):
                 print(" ", end="")
