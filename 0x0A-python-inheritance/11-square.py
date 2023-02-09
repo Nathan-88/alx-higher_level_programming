bee43930de040672f00c4820bdc08ee0cd9788c3
Nathan-88 <onwukaebuka88@gmail.com>
2023-02-09 11:33:09 +0100

    print() should print, and str() should return, the square description: [Square] <width>/<height>

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ represents a subclass square"""
+
+    def __init__(self, size):
+        """initializer for square"""
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """area of square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """print"""
+        return ("[Square] {:d}/{:d}".format(self.__size, self.__size))
