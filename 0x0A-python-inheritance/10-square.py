3ef3c57de8d1abb041ab2018f75d8689e2e351fd
Nathan-88 <onwukaebuka88@gmail.com>
2023-02-09 10:56:23 +0100

    subclass square with superclass Rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" repesents a square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ represents a subclass square"""
+
+    def __init__(self, size):
+        """initializer for square"""
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator("size",size)
+
+    def area(self):
+        """area of square"""
+        return self.__size ** 2
