88aba7e98bf2a177d4ee9260255d0adac127efd9
Nathan-88 <onwukaebuka88@gmail.com>
2023-02-09 08:58:59 +0100

    width and height must be positive integers, validated by integer_validator

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)"""
+
+
+class Rectangle:
+    """represents a rectangle"""
+
+    def __init__(self, width, height):
+        """initializer for rectangle"""
+        self.__width = width
+        self.__height = height
+
+    
+
+
