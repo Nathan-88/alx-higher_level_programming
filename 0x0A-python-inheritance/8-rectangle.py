586ec86b4014b833268390ee393e7bea4b4c6908
Nathan-88 <onwukaebuka88@gmail.com>
2023-02-09 09:45:43 +0100

    Write a class Rectangle that inherits from BaseGeometry (7-base_geometry.py).

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python3
 """a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry():
-    """defines class"""
-
-    def area(self):
-        """area not implemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """integer validator"""
-        if not isinstance(value, int):
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """represents a rectangle"""
