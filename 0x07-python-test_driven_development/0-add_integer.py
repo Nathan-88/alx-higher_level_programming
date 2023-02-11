5adbe3a63c4cb77b82fc157c7d8d9a7f5cd17d1f
Nathan-88 <onwukaebuka88@gmail.com>
2023-02-11 12:50:58 +0100

    a function that adds 2 integers.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""function returns sum of two integers"""
+"""
+This is the "0-add_integer" module.
+The 0-add_integer module supplies one function, add_integer(a, b).
+a and b must be int
+"""
 
 
 def add_integer(a, b=98):
-    """function that adds two integers"""
+    """function that adds two integers.
+    return: a+b
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
