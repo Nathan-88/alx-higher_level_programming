5cc146e35f111cd5b94ad19a1c89cdb966c4ee79
Nathan-88 <onwukaebuka88@gmail.com>
2023-02-13 08:15:19 +0100

    nction that prints My name is <first name> <last name>

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -12,4 +12,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    print("My name is {:s} {:s}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
