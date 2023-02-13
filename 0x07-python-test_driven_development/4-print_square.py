df66c859341170ddc195a66c2be57ea30eb24d71
Nathan-88 <onwukaebuka88@gmail.com>
2023-02-13 10:50:36 +0100

    a function that divides all elements of a matrix.

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a function that prints a square 
+with the character #.
+"""
+
+
+def print_square(size):
+    """
+    a function that prints a square
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    print("#" * size for i in range(size))
