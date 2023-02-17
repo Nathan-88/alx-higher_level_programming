17c4ebb3491f2da3b31bddbfe27e31705e106244
Nathan-88 <onwukaebuka88@gmail.com>
2023-02-17 19:46:03 +0100

    a class Student that defines a student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" a class Student that defines a student by:
+"""
+
+
+class Student():
+    """defines a class of student
+    """
+    def __init__(self, first_name, last_name, age):
+        """define instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves a dictionary representation
+        of a Student instance
+        """
+        return self.__dict__
