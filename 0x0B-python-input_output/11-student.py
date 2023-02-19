a6b81c7a255af81988e7e7e1a10adb1c2a162160
Nathan-88 <onwukaebuka88@gmail.com>
2023-02-19 06:28:08 +0100

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+class Student():
+    """defines a class of student
+    """
+    def __init__(self, first_name, last_name, age):
+        """define instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ If attrs is a list of strings, only attribute names
+        contained in this list must be retrieved.
+        Otherwise, all attributes must be retrieved
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            diction = {}
+            if isinstance(attrs, list):
+                if all(isinstance(attr, str) for attr in attrs):
+                    for i in attrs:
+                        if i in self.__dict__:
+                            diction.update({i: self.__dict__[i]})
+            return diction
