617fe9f3849372f0ca7a81dda371e33f95ca51fa
Nathan-88 <onwukaebuka88@gmail.com>
2023-03-03 10:30:12 +0100

    update by adding public method update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,3 +90,11 @@ class Rectangle(Base):
         """defines the string representation of rectangle"""
         return "[Rectangle] ({}) {}/{} - {}/{}"\
             .format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args):
+        """assigns an argument to each attribute:"""
+        self.id = args[0] if len(args) >=1 else self.id
+        self.width = args[1] if len(args) >= 2 else self.width
+        self.height = args[2] if len(args) >= 3 else self.height
+        self.x = args[3] if len(args) >= 4 else self.x
+        self.__y = args[4] if len(args) >= 5 else self.y
