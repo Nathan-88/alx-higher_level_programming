88e2e52fc96a545834fcd84c46969878520402ef
Nathan-88 <onwukaebuka88@gmail.com>
2023-03-16 07:49:22 +0100

    command that creates a new database with the name "hbtn_0c_0" if it doesn't already exist.

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
---command that creates a new database with the name "hbtn_0c_0" if it doesn't already exist.
+--creates a new database with the name "hbtn_0c_0" if it doesn't already exist.
 CREATE DATABASE hbtn_0c_0 IF NOT EXISTS `hbtn_0c_0`;
