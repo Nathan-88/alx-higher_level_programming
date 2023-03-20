eb9c05d92c9005692b52a972463fa7aecad06639
Nathan-88 <onwukaebuka88@gmail.com>
2023-03-20 14:55:55 +0100

    Creates the table hbtn_0d_usa with table states.

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- Creates the table hbtn_0d_usa with table states.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, `name` VARCHAR(256) NOT NULL);
