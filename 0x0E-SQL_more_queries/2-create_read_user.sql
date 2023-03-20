cfb1c092a21eec86785164d7532bddd08343b7ab
Nathan-88 <onwukaebuka88@gmail.com>
2023-03-20 10:51:57 +0100

    creates the database hbtn_0d_2 and the user user_0d_2 if not already exists

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- creates the database hbtn_0d_2 and the user user_0d_2 if not already exists.
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
