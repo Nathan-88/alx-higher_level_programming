370eb5fb45e7dd747cad5a9f4d0b8b52eb2c82e8
Nathan-88 <onwukaebuka88@gmail.com>
2023-04-13 10:19:13 +0100

    a script that prints the first argument passed to it:

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 // a script that prints the first argument passed to it:
 // If no arguments are passed to the script, print “No argument”
+
 const args = process.argv;
 if (args.length === 2) {
   console.log('No argument');
 } else {
   console.log(args[2]);
 }
+// console.log(process.argv[2] ? process.argv[2] : 'No argument');
