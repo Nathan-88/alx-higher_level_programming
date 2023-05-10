d0ddaca3da9d50a1fc4689fe31cd5bbfefdc77af
Nathan-88 <onwukaebuka88@gmail.com>
2023-05-10 08:00:19 +0100

    sends a JSON POST req to URL $!, display response body

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST req to URL $!, display response body
-curl -sI $1 -X POST -d @"$2" -H 'content-type: application/json'
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
