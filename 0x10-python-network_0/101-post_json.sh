385c8fe1e1669848a3e3f1290d5ef51536c51957
Nathan-88 <onwukaebuka88@gmail.com>
2023-05-10 08:34:50 +0100

    Your script must send a POST request with the contents of a file, passed with the filename as the second argument of the script, in the body of the request

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST req to URL $!, display response body
-curl -sI -H "content-type:application/json"  -d @"$2" -X POST "$1"
+curl -sI -H "content-type:application/json" -d @"$2" -X POST "$1"
