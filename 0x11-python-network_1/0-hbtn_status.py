74603757f38f74a93ab84511ae5e5e36d1355ad3
Nathan-88 <onwukaebuka88@gmail.com>
2023-05-11 09:54:29 +0100

    Write a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,14 +5,10 @@ Write a Python script that fetches https://alx-intranet.hbtn.io/status
 
 
 import urllib.request
-import chardet
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html_content = response.read()
-    content_type = response.headers.get('content-type')
-    charset = chardet.detect(html_content)['encoding']
-    html_content = html_content.decode(charset)
     print("Body response:")
-    print("\t- type: {}".format(type(html_content.encode('utf-8'))))
-    print("\t- content: {}".format(html_content.encode('utf-8')))
-    print("\t- utf8 content: {}".format(html_content.encode('utf-8').decode('utf-8')))
+    print("\t- type: {}".format(type(html_content)))
+    print("\t- content: {}".format(html_content))
+    print("\t- utf8 content: {}".format(html_content.decode('utf-8')))
