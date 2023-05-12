5530834bc3bcceac3af4052461de487206a597ec
Nathan-88 <onwukaebuka88@gmail.com>
2023-05-12 13:06:58 +0100

    a Python script that takes 2 arguments in order to solve this challenge.

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,13 +12,13 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2], sys.argv[1])
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2],
+                                                              sys.argv[1])
     r = requests.get(url)
     try:
         json = r.json()
         for i in range(10):
             print("{}: {}".format(json[i].get('sha'),
-                                  json[i].get('commit').get('author').
-                                  get('name')))
+                  json[i].get('commit').get('author').get('name')))
     except IndexError:
         pass
