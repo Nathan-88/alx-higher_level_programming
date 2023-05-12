9c8496c5d85035a491b30268bb526d62bf0e4375
Nathan-88 <onwukaebuka88@gmail.com>
2023-05-12 07:45:11 +0100

    a Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+r = requests.get('https://alx-intranet.hbtn.io/status')
+print("Body response:")
+print("\t- type: {}".format(type(r.text)))
+print("\t- content: {}".format(r.text))
