4fcf0e3fd4086138f126a8c885a8e77a09333411
Nathan-88 <onwukaebuka88@gmail.com>
2023-06-13 14:13:50 +0100

    a script that searches the second biggest integer in the list of arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 //  a script that searches the second biggest integer in the list of arguments.
 const num = process.argv;
-const new_num = [];
+const List = [];
 if (num.length <= 3) {
   console.log(0);
 } else {
   for (let i = 2; i < num.length; i++) {
-    new_num.push(parseInt(num[i]));
+    List.push(parseInt(num[i]));
   }
-  new_num.sort(function (a, b) { return a - b; });
-  console.log(new_num[new_num.length - 2]);
+  List.sort(function (a, b) { return a - b; });
+  console.log(List[List.length - 2]);
 }
