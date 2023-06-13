81eaf845fe658d1a2284a079a902b1f00452e059
Nathan-88 <onwukaebuka88@gmail.com>
2023-06-13 12:33:08 +0100

    a script that prints the addition of 2 integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// a script that prints the addition of 2 integers
+
+function add(a, b) {
+    return a + b;
+}
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+console.log(add(a, b));
