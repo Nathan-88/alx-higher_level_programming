5b9bab097fe379cbf00bb4c7d6e5ba51c89dd6d2
Nathan-88 <onwukaebuka88@gmail.com>
2023-06-14 10:06:21 +0100

    a class Rectangle that defines a rectangle initializes the instance attr

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+//  a class Rectangle that defines a rectangle initializes the instance attr
+class Rectangle {
+  constructor (w, h) {
+    this.height = h;
+    this.width = w;
+  }
+}
+
+module.exports = Rectangle;
