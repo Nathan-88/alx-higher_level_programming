24b31c68500ea0688991176936fff0b31c7b18a3
Nathan-88 <onwukaebuka88@gmail.com>
2023-06-14 12:53:15 +0100

    class resctancle with validators

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return {};
+    }
+  }
+}
+
+module.exports = Rectangle;
