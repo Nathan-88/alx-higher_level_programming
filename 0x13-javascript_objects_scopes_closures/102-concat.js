eff6bb1637e4a8b2a8e3f314419974594188dd1e
Nathan-88 <onwukaebuka88@gmail.com>
2023-06-15 21:01:21 +0100

    a script that concats 2 files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -9,14 +9,8 @@ const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 const textA = fs.readFileSync(fileA, 'utf8');
-// read from file A and store in variable "textA"
-// console.log("File A: ", textA); //print out File A to console for debugging purposes
+
 const textB = fs.readFileSync(fileB, 'utf8');
-// read from file B and store in variable "textB"
-// console.log("File B: ", textB); //print out File B to console for debugging purposes
+
 const newText = `${textA}\n${textB}`;
-// concatenate textA and textB and store in variable "newText"
-// console.log("New Text: ", newText); //print out New Text to console for debugging purposes
 fs.writeFileSync(fileC, newText);
-// write newText to fileC
-// console.log("File C: ", fileC); //print out File C to console for debugging purposes
