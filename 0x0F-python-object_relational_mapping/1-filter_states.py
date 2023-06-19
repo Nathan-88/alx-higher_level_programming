4d81c395735a9f75fe164e9cee3dbac5546cfd0e
Nathan-88 <onwukaebuka88@gmail.com>
2023-06-19 10:24:33 +0100

    a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-# a script that lists all states with a name starting with N
-# (upper N) from the database hbtn_0e_0_usa
+"""
+a script that lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa
+You must use the module MySQLdb (import MySQLdb)
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+Results must be displayed as they are in the example below
+Your code should not be executed when imported
+"""
 import MySQLdb
 import sys
 
@@ -11,7 +18,7 @@ if __name__ == '__main__':
     c = db.cursor()
     c.execute(
         """SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER
-        BY states.id""")
+        BY id""")
     rows = c.fetchall()
     for row in rows:
         print(row)
