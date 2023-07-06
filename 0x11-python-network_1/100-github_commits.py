39c7435024e8f13adcb2bffcbf814ec2b9ae36c0
Nathan-88 <onwukaebuka88@gmail.com>
2023-07-06 20:27:28 +0100

    technical interview question

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+a Python script that takes 2 arguments in order to solve this challenge
+The first argument will be the repository name
+The second argument will be the owner name
+You must use the packages requests and sys
+You are not allowed to import packages other than requests and sys
+You don't need to check arguments passed to the script (number or type)
+"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        sys.argv[2], sys.argv[1])
+    r = requests.get(url)
+    try:
+        json_data = r.json()
+        for i in range(10):
+            print("{}: {}".format(json_data[i].get('sha'),
+                  json_data[i].get('commit').get('author').get('name')))
+    except IndexError:
+        pass
