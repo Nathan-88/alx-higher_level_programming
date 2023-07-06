fe4d34598827f78bf546eeb7b319d37c7aaa4f8b
Nathan-88 <onwukaebuka88@gmail.com>
2023-07-06 18:15:35 +0100

    a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -15,7 +15,7 @@ import requests
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    if sys.argv[1]:
+    if len(sys.argv) > 1:
         value = sys.argv[1]
     else:
         value = ""
