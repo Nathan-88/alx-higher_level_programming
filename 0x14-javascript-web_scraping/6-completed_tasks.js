d226876a44d1f2ecbe857df0999b938bf2e57b90
Nathan-88 <onwukaebuka88@gmail.com>
2023-07-25 18:58:05 +0100

    a script that computes the number of tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,23 +2,17 @@
 //  a script that computes the number of tasks
 // completed by user id.
 const request = require('request');
-const url = process.argv[2];
-
-request.get(url, { json: true }, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  const tasksCompleted = {};
-  body.forEach((todo) => {
-    if (todo.completed) {
-      if (!tasksCompleted[todo.userId]) {
-        tasksCompleted[todo.userId] = 1;
-      } else {
-        tasksCompleted[todo.userId] += 1;
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
-    }
-  });
-  console.log(tasksCompleted);
+    });
+    console.log(completed);
+  }
 });
