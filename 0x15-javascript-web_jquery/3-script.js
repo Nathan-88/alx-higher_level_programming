9b8f94ae0884d5de7520c3d948491d1d5582bb00
Nathan-88 <onwukaebuka88@gmail.com>
2023-08-04 11:24:43 +0100

    Write a JavaScript script that adds the class red to the <header> element when the user clicks on the tag DIV#red_header

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/**
+ * Write a JavaScript script that adds the class red to the <header> element when the user clicks
+ * on the tag DIV#red_header
+ */
+$(document).ready(function () {
+  $('#red_header').click(function () {
+    $('header').addClass('red');
+  });
+});
