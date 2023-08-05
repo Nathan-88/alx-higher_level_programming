11bfb8ce60ecba83dc7bc26527d86a8ca21cdca2
Nathan-88 <onwukaebuka88@gmail.com>
2023-08-05 11:37:30 +0100

    a JavaScript script that fetches and prints how to say “Hello” depending on the language

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -3,20 +3,21 @@
  * a JavaScript script that fetches and prints how to say “Hello” depending on the language
  */
 $(document).ready(function () {
-	$("INPUT#btn_translate").click(fetchTranslation);
-	$("INPUT#language_code").keyup(function (event) {
+	$("input#btn_translate").click(fetchTranslation);
+	$("input#language_code").keyup(function (event) {
 		if (event.keyCode === 13) {
 			fetchTranslation();
 		}
 	});
 
 	function fetchTranslation() {
-		const language_code = $("INPUT#language_code").val();
-		$.getJSON(
-			`https://hellosalut.stefanbohacek.dev/?lang=${languageCode}`,
-			function (data) {
+		const languageCode = $("input#language_code").val();
+		$.getJSON(`https://hellosalut.stefanbohacek.dev/hello/?lang=${languageCode}`, function (data) {
 				$("#hello").text(data.hello);
 			}
-		);
+		)
+		.fail(function () {
+			$("#hello").text('Translation failed. Please try again later.');
+		});
 	}
 });
